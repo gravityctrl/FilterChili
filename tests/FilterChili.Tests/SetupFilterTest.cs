eb9e881aea64570d811a91d964d7de969f2b95c0
gravityctrl <hybrid.art@live.de>
2017-12-03 19:49:28 +0100

    [-] Remove unused using directive.

--- tests/FilterChili.Tests/SetupFilterTest.cs
+++ tests/FilterChili.Tests/SetupFilterTest.cs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public 
 // License along with FilterChili. If not, see <http://www.gnu.org/licenses/>.
 
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Bogus;
