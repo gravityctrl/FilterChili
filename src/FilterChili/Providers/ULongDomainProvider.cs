1587824304437cfc0ac0803da6ac1b548045aa51
gravityctrl <hybrid.art@live.de>
2017-12-17 21:28:31 +0100

    [+] Add missing domain providers.

---
+++ src/FilterChili/Providers/ULongDomainProvider.cs
@@ -0,0 +1,34 @@
+﻿// This file is part of FilterChili.
+// Copyright © 2017 Sebastian Krogull.
+// 
+// FilterChili is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as 
+// published by the Free Software Foundation, either version 3 
+// of the License, or any later version.
+// 
+// FilterChili is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public 
+// License along with FilterChili. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Linq.Expressions;
+using GravityCTRL.FilterChili.Resolvers.Range;
+
+namespace GravityCTRL.FilterChili.Providers
+{
+    public class ULongDomainProvider<TSource> : DomainProvider<TSource, ulong>
+    {
+        internal ULongDomainProvider(Expression<Func<TSource, ulong>> selector) : base(selector) { }
+
+        public ULongRangeResolver<TSource> Range(string name, Action<ULongRangeResolver<TSource>> options = null)
+        {
+            var resolver = new ULongRangeResolver<TSource>(name, Selector);
+            options?.Invoke(resolver);
+            return resolver;
+        }
+    }
+}
