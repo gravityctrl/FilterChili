f7ef516af822ce63728be52e5459cf4cfbf576a0
gravityctrl <hybrid.art@live.de>
2018-03-15 23:49:24 +0100

    [i] Improve code.

--- src/FilterChili/Extensions/TypeExtensions.cs
+++ src/FilterChili/Extensions/TypeExtensions.cs
@@ -16,7 +16,6 @@
 
 using System;
 using System.Linq;
-using System.Text;
 
 namespace GravityCTRL.FilterChili.Extensions
 {
@@ -32,7 +31,7 @@ namespace GravityCTRL.FilterChili.Extensions
             }
 
             var name = type.Name.Substring(0, type.Name.LastIndexOf(GENERIC_MARKER, StringComparison.Ordinal));
-            var genericArguments = string.Join(",", type.GetGenericArguments().Select(FormattedName));
+            var genericArguments = string.Join(',', type.GetGenericArguments().Select(FormattedName));
             return $"{name}<{genericArguments}>";
         }
     }
